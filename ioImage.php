4919a2074ee40d032c4093abfb97f710c28c8408
kitaz <kitaz@04cb49c0-64cb-e311-98bc-902b34dfe7c8>
2014-05-11 10:56:15 +0000

    "svn://192.168.0.7/KitazDao" にプロジェクト "KitazDao" を共用

---
+++ ioImage.php
@@ -0,0 +1,71 @@
+<?php
+// KitazDao using variants, class files
+require_once __DIR__ .'/KitazDao/kitazDao.class.php';
+define("KD_DAO_PATH", __DIR__ ."/dao");
+define("KD_ENTITY_PATH", __DIR__ ."/entity");
+
+// ファイルアップロード用
+$file = @$_FILES['img'];
+$alt = @$_POST['alt'];
+
+// ファイル削除用
+$delId = @$_POST['iid'];
+
+// upload処理
+if (isset($file)){
+	$mime = $file['type'];
+	$path = $file['tmp_name'];
+	$pathInfo = pathinfo($path);
+	$fileName = $pathInfo["basename"];
+	$size = $file['size'];
+	$error = $file['error'];
+	$imgInfo = getimagesize($path);
+// 	echo $imgInfo[0] . "/". $imgInfo[1];
+	$fileImg = file_get_contents($path);
+
+	$kd = new KitazDao("Table1Dao");
+	$dto = new Table1Dto();
+	
+	$kd->begintrans();
+	$maxArr = $kd->getMaxTid();
+	$maxTid = 0;
+	if ($maxArr[0]["mtid"] !== null){
+		$maxTid = (int)$maxArr[0]["mtid"] +1;
+	}
+	$dto->setTid($maxTid);
+	$dto->setTname($fileName);
+	$dto->setAttribute(0);
+	$dto->setTcomment($alt ."\n". $imgInfo[0] ."/". $imgInfo[1]);
+	$dto->setTimage($fileImg);
+	$kd->insertTable($dto);
+	$kd->commit();
+
+// 	$kd->begintrans();
+// 	$maxTid = 2;
+// 	$dto->setTid($maxTid);
+// 	$dto->setTname($fileName);
+// 	$dto->setAttribute(2);
+// 	$dto->setTcomment($alt ."\n". $imgInfo[0] ."/". $imgInfo[1]);
+// 	$dto->setTimage($fileImg);
+// 	$kd->updateTable($dto);
+// 	$kd->commit();
+
+	$maxArr = $kd->getMaxTid();
+	$retArr = $kd->seletTable($maxTid);
+	var_dump($retArr);
+	
+// 	$kd->begintrans();
+// 	$dto->setTid(3);
+// 	$kd->deleteTable($dto);
+// 	$kd->commit();
+	
+	
+}
+
+// 削除用
+if (isset($delId)){
+	
+	
+}
+
+?>
