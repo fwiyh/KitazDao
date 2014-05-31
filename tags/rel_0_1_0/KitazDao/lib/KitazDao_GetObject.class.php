92839087ef8d0b42284721b134069feee3922449
kitaz <kitaz@04cb49c0-64cb-e311-98bc-902b34dfe7c8>
2014-05-31 13:26:53 +0000

    rel 0.1.0

---
+++ tags/rel_0_1_0/KitazDao/lib/KitazDao_GetObject.class.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once __DIR__ .'/KitazDaoBase.class.php';
+
+/**
+ * KitazDao
+ * @name KitazDao_GetObject
+ * @author keikitazawa
+ */
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 keikitazawa
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+class KitazDao_GetObject extends KitazDaoBase {
+	
+	/**
+	 * Dao名からDaoクラスを返す
+	 * @param String $className Daoクラス名
+	 * @return variant 取得失敗:false 取得成功：Daoクラス
+	 */
+	public function getDaoClass($className){
+		try {
+			require_once KD_DAO_PATH ."/". $className .".class.php";
+			return new $className();
+		}catch (Exception $e) {
+			echo parent::getCustomExceptionMessage(5);
+			return false;
+		}
+	}
+	
+	/**
+	 * Daoに定義されているBEANからEntity名を取得する
+	 * @param Object $daoClass
+	 * @return variant 取得失敗:false 取得成功：String Entity名
+	 */
+	public function getEntityNameByDao($daoClass){
+		try {
+			return $daoClass::BEAN;
+		} catch(Exception $e){
+			echo parent::getCustomExceptionMessage(7);
+			return false;
+		}
+	}
+	
+	/**
+	 * Entity名からEntityクラスを返す
+	 * @param String $EntityName
+	 * @return variant 取得失敗:false 取得成功：Dtoクラス
+	 */
+	public function getEntityClass($entityName){
+		try {
+			require_once KD_ENTITY_PATH ."/". $entityName .".class.php";
+			return new $entityName();
+		}catch (Exception $e) {
+			echo parent::getCustomExceptionMessage(6);
+			return false;
+		}
+	}
+}
