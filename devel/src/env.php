eb2c82cb7f8cece9d62f09ab44a67b41b2939ccb
kitaz <kitaz@04cb49c0-64cb-e311-98bc-902b34dfe7c8>
2015-03-15 17:31:26 +0000

    開発体制の再構築　作業分

---
+++ devel/src/env.php
@@ -0,0 +1,11 @@
+<?php
+require_once "../../KitazDao/kitazDao.class.php";
+define("KD_DAO_PATH", "../dao");
+define("KD_ENTITY_PATH", "../dto");
+
+define("MYSQL_CONFIG", "KitazDao.mysql.config");
+define("SQLSRV_CONFIG", "KitazDao.sqlsrv.config");
+define("ORA12C_CONFIG", "KitazDao.ora12c.config");
+define("PGSQL_CONFIG", "KitazDao.pgsql.config");
+define("ACCDB_CONFIG", "KitazDao.accdb.config");
+define("SQLITE3_CONFIG", "KitazDao.sqlite3.config");
