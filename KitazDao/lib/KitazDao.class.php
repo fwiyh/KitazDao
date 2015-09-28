42b6e3017eca8125950a5dc09c5a8623794eba81
kitaz <kitaz@04cb49c0-64cb-e311-98bc-902b34dfe7c8>
2015-09-28 15:28:30 +0000

    0.6.0-alpha ディレクトリセパレーターがある場合の設定ファイルの場所は、記述されたものそのままに ソース内MIT条項の削除

--- KitazDao/lib/KitazDao.class.php
+++ KitazDao/lib/KitazDao.class.php
@@ -7,29 +7,6 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . "KitazDaoBase.class.php";
  * @name KitazDao
  * @author keikitazawa
  */
-// ----------------------------------------------------------------------------
-// The MIT License (MIT)
-//
-// Copyright (c) 2014 keikitazawa
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 class KitazDao extends KitazDaoBase {
 	
 	/**
@@ -118,8 +95,15 @@ class KitazDao extends KitazDaoBase {
 		// 戻り値
 		$ret = array();
 		
+		// 0.6.0 ディレクトリセパレートがある場合はそのままのパスを渡す
+		$dsnstr = $cfgName;
+		if (strpos($dsnstr, DIRECTORY_SEPARATOR) === false){
+			// KitazDaoディレクトリ直下を参照する
+			$dsnstr = substr(__DIR__, 0, strrpos(__DIR__, DIRECTORY_SEPARATOR)) . DIRECTORY_SEPARATOR . $dsnstr;
+		}
+		
 		// 設定ファイルを読み込む
-		$iniArr = parse_ini_file(substr(__DIR__, 0, strrpos(__DIR__, DIRECTORY_SEPARATOR)) . DIRECTORY_SEPARATOR . $cfgName, true);
+		$iniArr = parse_ini_file($dsnstr, true);
 		if ($iniArr === false){
 			throw new KitazDaoException(1);
 		}
