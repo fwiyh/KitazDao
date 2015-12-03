90aa4dc42a2d43c2a82078cdf8fb02e3676c7f92
kitaz <kitaz@04cb49c0-64cb-e311-98bc-902b34dfe7c8>
2015-12-03 16:02:03 +0000

    作業分更新

--- devel/src/env.php
+++ devel/src/env.php
@@ -1,10 +1,11 @@
 <?php
 require_once "../../KitazDao/kitazDao.class.php";
 
-define("MYSQL_CONFIG", "KitazDao.mysql.config");
-define("SQLSRV_CONFIG", "KitazDao.sqlsrv.config");
-define("ORA12C_CONFIG", "KitazDao.ora12c.config");
-define("PGSQL_CONFIG", "KitazDao.pgsql.config");
-define("ACCDB_CONFIG", "KitazDao.accdb.config");
-define("SQLITE3_CONFIG", "KitazDao.sqlite3.config");
-define("DB2_CONFIG", "KitazDao.db2.config");
+define("DB_CONFIG_DIR", "..". DIRECTORY_SEPARATOR ."config". DIRECTORY_SEPARATOR);
+
+define("MYSQL_CONFIG", DB_CONFIG_DIR ."KitazDao.mysql.config");
+define("SQLSRV_CONFIG", DB_CONFIG_DIR ."KitazDao.sqlsrv.config");
+define("ORA12C_CONFIG", DB_CONFIG_DIR ."KitazDao.ora12c.config");
+define("PGSQL_CONFIG", DB_CONFIG_DIR ."KitazDao.pgsql.config");
+define("ACCDB_CONFIG", DB_CONFIG_DIR ."KitazDao.accdb.config");
+define("SQLITE3_CONFIG", DB_CONFIG_DIR ."KitazDao.sqlite3.config");
