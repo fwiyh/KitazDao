5fdbc1df90f7e58fa7980f6f565a3c6a22fd1f63
kitaz <kitaz@04cb49c0-64cb-e311-98bc-902b34dfe7c8>
2016-10-19 13:48:31 +0000

    テスト環境の新規構築のためのソース整理

--- devel/src/env.php
+++ devel/src/env.php
@@ -9,3 +9,28 @@ define("ORA12C_CONFIG", DB_CONFIG_DIR ."KitazDao.ora12c.config");
 define("PGSQL_CONFIG", DB_CONFIG_DIR ."KitazDao.pgsql.config");
 define("ACCDB_CONFIG", DB_CONFIG_DIR ."KitazDao.accdb.config");
 define("SQLITE3_CONFIG", DB_CONFIG_DIR ."KitazDao.sqlite3.config");
+
+// アクセスファイルのファイルシステムパス
+define("FILESYSTEM_CURRENT_DIRECTORY", __DIR__);
+// URLパス
+define("URI_CURRENT_DIRECTORY", $_SERVER['PHP_SELF']);
+// file名
+define("URI_FILE_NAME", pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME));
+// 拡張子
+define("URI_FILE_EXTENSION", pathinfo($_SERVER['PHP_SELF'], PATHINFO_EXTENSION));
+
+// dao定数 
+if ($_SESSION["SelectDatabase"] == "accdb"){
+	define("KD_DAO_PATH", "../dao_mdb");
+}else {
+	define("KD_DAO_PATH", "../dao");
+}
+// dto定数
+if ($_SESSION["SelectDatabase"] == "olacle"){
+	define("KD_ENTITY_PATH", "../dto_ora");
+}else if ($_SESSION["SelectDatabase"] == "sqlsvr"){
+	define("KD_ENTITY_PATH", "dto_sqlsrv");
+}else {
+	define("KD_ENTITY_PATH", "../dto");
+}
+
---
+++ devel/src/index.php
@@ -0,0 +1,16 @@
+<?php
+// KitazDao using variants, class files
+require_once "./env.php";
+//define("KD_DEBUG_SQL_OUTPUT_PATH", "1.php.txt");
+
+//$kd = new KitazDao(MYSQL_CONFIG);
+//$kd = new KitazDao(SQLSRV_CONFIG);
+//$kd = new KitazDao(ORA12C_CONFIG);
+//$kd = new KitazDao(PGSQL_CONFIG);
+//$kd = new KitazDao(ACCDB_CONFIG);
+//$kd = new KitazDao(SQLITE3_CONFIG);
+
+
+
+
+
