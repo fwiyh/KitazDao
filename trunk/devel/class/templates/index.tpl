ff04bf9b0b5ab3562a9154ae29772421e21a012a
kitaz <kitaz@04cb49c0-64cb-e311-98bc-902b34dfe7c8>
2016-10-25 15:28:10 +0000

    作業分更新

--- trunk/devel/class/templates/index.tpl
+++ trunk/devel/class/templates/index.tpl
@@ -1,14 +1,20 @@
 <html>
 <head>
 	<title>メニュー</title>
-	<script type="script/javascript" src="../js/jquery-3.1.1.js"></script>
-	<script type="script/javascript" src="../js/SessionControl.prot.js"></script>
-	<script type="script/javascript" src="../js/index.js"></script>
-	
+	<script type="text/javascript" src="./js/jquery-3.1.1.js"></script>
+	<script type="text/javascript" src="./js/SessionControl.prot.js"></script>
+	<script type="text/javascript" src="./js/index.js"></script>
+	<script>
+	$(document).ready(
+		function(){
+			SetEvents();
+		}
+	);
+	</script>
 </head>
 <body>
-	対象DB{$selectDatabase}<br>
-	{html_options id="selectableDatabase" name="selectableDatabase" options=$selectableDatabase selected=$selectDatabase}
+	対象DB:{$selectDatabase}<br>
+	{html_options id="SelectableDatabase" name="selectableDatabase" options=$selectableDatabase selected=$selectDatabase}
 	<input type="button" id="ChangeDatabase" value="選択" />
 </body>
 </html>
--- trunk/devel/src/js/SessionControl.prot.js
+++ trunk/devel/src/js/SessionControl.prot.js
@@ -6,31 +6,35 @@ SessionControl = function(){
 	
 };
 
-SessionProc.prototype.setSession = function(name, value){
+SessionControl.prototype.setSession = function(name, value){
 	$.ajax({
 		async: true,
 		cache: false,
 		url: "./_SetSession.php",
+		type: "post",
+		dataType: "json",
 		data: {
 			name: name,
 			value: value
 		}
 	}).done(function(data, textStatus, jqXHR){
-		
+		alert("complete");
 	}).fail(function(data, textStatus, errorThrown){
-		
+		alert(data.status + "\n" + textStatus + "\n" + errorThrown.message);
 	}).always(function(data, textStatus, returnedObject){ //以前のcompleteに相当。ajaxの通信に成功した場合はdone()と同じ、失敗した場合はfail()と同じ引数を返します。
 		
 	});
 };
-SessionProc.prototype.getSession = function($name){
+SessionControl.prototype.getSession = function(name){
 	
 };
-SessionProc.prototype.closeSession = function($name){
+SessionControl.prototype.closeSession = function(name){
 	$.ajax({
 		async: true,
 		cache: false,
 		url: "./Unsetsession.php",
+		type: "post",
+		dataType: "json",
 		data: {
 			name: name
 		}
